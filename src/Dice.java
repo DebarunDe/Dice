5111180e22ff6d6ce5f85b89784c301b8a148e92
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2018-10-01 14:28:44 -0400

    Add files via upload

---
+++ src/Dice.java
@@ -0,0 +1,28 @@
+
+public class Dice {
+	
+	private int numRolls;
+	
+	public Dice() {
+		
+		numRolls = 0;
+	}
+	
+	public int roll() {
+		
+		numRolls++;
+		return (int)(Math.random() * 6) + 1;
+	}
+	
+	public void reset() {
+		
+		numRolls = 0;
+	}
+	
+	public int getNumRoll() {
+		
+		return numRolls;
+		
+	}
+
+}
---
+++ src/DiceMain.java
@@ -0,0 +1,11 @@
+
+public class DiceMain {
+
+	public static void main(String[] args) {
+	
+		Dice dice = new Dice();
+		System.out.println(dice.roll());
+
+	}
+
+}
